b1269b612b706ba74becfd190053e31aed9f0568
wells63 <bsc-com-28-23@unima.ac.mw>
2025-04-24 16:37:48 +0200

    Add files via upload

---
+++ LAB9 CASTING/Base.h
@@ -0,0 +1,6 @@
+#ifndef BASE_H
+#define BASE_H
+class Base { 
+    virtual void DummyFunction(); 
+    };
+    #endif
---
+++ LAB9 CASTING/Derived.h
@@ -0,0 +1,10 @@
+#ifndef DERIVED_H
+#define DERIVED_H
+#include "Base.h"
+class Derived : public Base { 
+    public: 
+    Derived(); 
+    private: 
+    int a; 
+    };
+    #endif
